914db61810558a89c211dd4100d3d2ede8c63161
sajiquo <szmckw@gmail.com>
2022-01-08 07:28:15 +0000

    modify slider change and display value by slide

--- src/ui/slider/Slider.stories.tsx
+++ src/ui/slider/Slider.stories.tsx
@@ -9,3 +9,29 @@ export default {
 const Template: ComponentStory<typeof Slider> = (args) => <Slider {...args} />;
 
 export const Default = Template.bind({});
+Default.args = {
+  min: 40,
+  max: 240,
+  step: 1,
+};
+
+export const ValidationNegative = Template.bind({});
+ValidationNegative.args = {
+  min: -40,
+  max: -240,
+  step: -1,
+};
+
+export const ValidationInvert = Template.bind({});
+ValidationInvert.args = {
+  min: 240,
+  max: 40,
+  step: 1,
+};
+
+export const ValidationFloat = Template.bind({});
+ValidationFloat.args = {
+  min: 240.22,
+  max: 40.333,
+  step: 1.444,
+};
--- src/ui/slider/Slider.tsx
+++ src/ui/slider/Slider.tsx
@@ -2,27 +2,80 @@ import { useMemo, useState } from "react";
 import type { FC } from "react";
 import { Circle } from "~/ui/circle/Circle";
 import { slider_circle, slider_root, slider_slide } from "./Slider.module.css";
+import { useCallback } from "react";
 
-const getClipPathStr = (fillRate: number) =>
-  `polygon(0% 100%, 100% 100%, 100% ${fillRate}%, 0% ${fillRate}%)`;
+export interface SliderProps {
+  min: number;
+  max: number;
+  step: number;
+}
 
-export const Slider: FC = () => {
+export const Slider: FC<SliderProps> = (props) => {
+  const { min, max, step } = validate(props);
   const [fillRate, setFillRate] = useState(100);
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const getValue = useCallback(
+    (fillrate: number) => {
+      const range = max - min;
+      const multipledRate = (fillrate * range) / 100;
+      const roundedRate = multipledRate - (multipledRate % step);
+      return roundedRate + min;
+    },
+    [fillRate, min, max, step]
+  );
   const clipPath = useMemo(() => getClipPathStr(fillRate), [fillRate]);
+
   return (
     <div
       className={slider_root}
-      onClick={() => {
-        setFillRate(fillRate - 10);
+      onMouseDown={() => setIsMouseDown(true)}
+      onMouseUp={() => setIsMouseDown(false)}
+      onMouseMove={(ev) => {
+        if (!isMouseDown) return;
+        const { currentTarget, pageY } = ev;
+        requestAnimationFrame(() => {
+          const { y, height } = currentTarget.getBoundingClientRect();
+          setFillRate(calcFillRate(pageY - y - window.scrollY, height));
+        });
       }}
     >
-      <Circle className={slider_circle}>circle1</Circle>
+      <Circle className={slider_circle}>{getValue(fillRate)}</Circle>
       <Circle
         className={[slider_slide, slider_circle].join(" ")}
         style={{ clipPath }}
       >
-        circle2
+        {getValue(fillRate)}
       </Circle>
     </div>
   );
 };
+
+/**
+ * force positive int
+ */
+const validate = (props: SliderProps) => {
+  const { min, max, step } = props;
+  const _min = Math.round(Math.abs(min));
+  const _max = Math.round(Math.abs(max));
+  const _step = Math.round(Math.abs(step)) || 1;
+  return _min < _max
+    ? {
+        min: _min,
+        max: _max,
+        step: _step,
+      }
+    : {
+        min: _max,
+        max: _min,
+        step: _step,
+      };
+};
+
+/**
+ * position by percentage from bottom
+ */
+const calcFillRate = (posFromTop: number, height: number): number =>
+  ((height - posFromTop) / height) * 100;
+
+const getClipPathStr = (fillRate: number) =>
+  `polygon(0% 100%, 100% 100%, 100% ${100 - fillRate}%, 0% ${100 - fillRate}%)`;
